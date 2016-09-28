298eb461223f0e890e68a9796cb3367bd18ccc97
Ashish T <ashish2py@gmail.com>
2016-09-28 17:28:19 +0530

    initial commit

---
+++ app/src/main/java/com/dotorbit/projectell/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.dotorbit.projectell;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dotorbit.projectell.main.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler activityHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        activityHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+            }
+        }, 5000);
+
+    }
+}
